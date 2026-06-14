03d2944c3dcd7eb320d9d5c30258232e2c85678e
fekuoe <felipe.viegas@ufcspa.edu.br>
2026-06-14 20:44:14 -0300

    agora vai funcionar tudo

--- backend/src/main/java/com/ufcspa/santa_rita_dashboard_service/config/SecurityConfig.java
+++ backend/src/main/java/com/ufcspa/santa_rita_dashboard_service/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "https://*.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Content-Type", "Authorization" , "Accept"));
         config.setAllowCredentials(true);
